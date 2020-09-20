ea49f2b86edc2de989b6318f601d08f8c4ff7543
thecodeah <codeah@protonmail.com>
2020-09-20 10:55:54 +0200

    Update docker-compose setup to make development easier

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,6 @@
-FROM golang:onbuild
+FROM golang:1.15.0-alpine
+
+WORKDIR /usr/src/app
+COPY . .
+RUN go build -o main .
+CMD ["./main"]
---
+++ run.sh
@@ -0,0 +1,5 @@
+case "$1" in
+    "dev") docker-compose up;;
+    "prod") docker-compose -f docker-compose.yml -f docker-compose.prod.yml up;;
+    *) echo "Usage: $0 dev || prod"
+esac
